c53542c4ed33b06bdc0ec2f1c0e2e3082e9ff894
mjourdan_cp <SND\mjourdan_cp>
2012-11-09 17:10:13 +0000

    moved the version number up

--- Dependencies/SharedAssemblyInfo.cs
+++ Dependencies/SharedAssemblyInfo.cs
@@ -4,6 +4,6 @@
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("Coding4Fun.DiceShaker")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
