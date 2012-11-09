921ef203ef762508eda5d294ea60b773cc3ed477
Mark Jourdan <mjourdan@markjourdan.com>
2012-11-09 16:56:52 +0000

    fixed an issue with the risk control not allowing you to change configurations, always going back to 1vs1

--- DiceShaker/MainPage.xaml.cs
+++ DiceShaker/MainPage.xaml.cs
@@ -12,43 +12,51 @@ namespace Coding4Fun.DiceShaker
     {
         private readonly Accelerometer _accelSensor = new Accelerometer();
         private MainPagePresenter _presenter;
+        private bool IsInit { get; set; }
 
         // Constructor
         public MainPage()
         {
             InitializeComponent();
-		}
+            IsInit = true;
+        }
 
 		protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
 		{
-			_presenter = new MainPagePresenter(this);
-			_presenter.Initialize();
+            if (IsInit)
+            {
+                _presenter = new MainPagePresenter(this);
+                _presenter.Initialize();
+                IsInit = false;
+            }
 		}
 
         #region IMainPageView Members
 
         public void LoadDynamicControl(UserControl control)
         {
-			Content.Children.Clear();
+            Content.Children.Clear();
             Content.Children.Add(control);
         }
 
         public void InitializeAccelerometer()
         {
-            _accelSensor.ReadingChanged += AccelerometerReadingChanged;
+            _accelSensor.CurrentValueChanged += AccelerometerReadingChanged;
             _accelSensor.Start();
         }
 
         #endregion
 
-        private void AccelerometerReadingChanged(object sender, AccelerometerReadingEventArgs e)
+        private void AccelerometerReadingChanged(object sender, SensorReadingEventArgs<AccelerometerReading> sensorReadingEventArgs)
         {
-            _presenter.AccelerometerReading(e.X, e.Y, e.Z);
+
+            _presenter.AccelerometerReading(sensorReadingEventArgs.SensorReading.Acceleration.X, sensorReadingEventArgs.SensorReading.Acceleration.Y, sensorReadingEventArgs.SensorReading.Acceleration.Z);
         }
 
         private void ApplicationBarIconButton_Click(object sender, EventArgs e)
         {
             NavigationService.Navigate(new Uri("/SettingsPage.xaml", UriKind.RelativeOrAbsolute));
+            IsInit = true;
         }
 
         private void OnFlick(object sender, FlickGestureEventArgs e)
