2b418d2bcce8cad01571eddc1ec9b661d8e8a165
Mark Jourdan <mjourdan@markjourdan.com>
2014-01-20 21:49:25 +0000

    fixed the about page to link to the code4fun page upgraded the nuget packages

--- DiceShaker/About.xaml.cs
+++ DiceShaker/About.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Tasks;
 
 namespace Coding4Fun.DiceShaker
 {
@@ -7,6 +10,12 @@ namespace Coding4Fun.DiceShaker
         public About()
         {
             InitializeComponent();
+        }
+
+        private void Code4FunImageTap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            var browserTask = new WebBrowserTask {Uri = new Uri("http://www.coding4fun.com")};
+            browserTask.Show();
         }
     }
 }
